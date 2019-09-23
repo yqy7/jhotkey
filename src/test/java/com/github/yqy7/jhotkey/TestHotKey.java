0f28e9734efec229240e4c061a2d8e42ae7d9a10
qiyun.yqy <qiyun.yqy@gmail.com>
2019-09-24 02:36:00 +0800

    opti

--- src/test/java/com/github/yqy7/jhotkey/TestHotKey.java
+++ src/test/java/com/github/yqy7/jhotkey/TestHotKey.java
@@ -1,22 +1,7 @@
 package com.github.yqy7.jhotkey;
 
-import java.awt.*;
-
-import javax.swing.*;
-
 public class TestHotKey {
     public static void main(String[] args) throws InterruptedException {
-        // -Djava.awt.headless=true设置了也会运行失败
-        //Toolkit
-        //KeyStroke.getKeyStroke("control J"); // 需要加载awt库，否则在mac下不工作
-
-        //Thread thread = new Thread(() -> {
-        //    //System.loadLibrary("awt"); // 本质上是需要加载awt库
-        //    KeyStroke.getKeyStroke("control J");
-        //    System.out.println("加载awt库");
-        //});
-        //thread.start();
-        //thread.join();
 
         HotKeyRegister instance = HotKeyRegister.getInstance();
         instance.register("ctrl J", hotKey -> {
